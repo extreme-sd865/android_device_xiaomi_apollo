63f2e98d71adfb3ee4582a5d2dc152575174fda3
Maru <maru@maru.ga>
2022-12-29 20:42:02 +0800

    init: fix init for 2 prime cores

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -5306,11 +5306,11 @@ case "$target" in
 	echo 3 > /sys/devices/system/cpu/cpu4/core_ctl/task_thres
 
 	# Core control parameters for gold+
-	echo 0 > /sys/devices/system/cpu/cpu7/core_ctl/min_cpus
-	echo 60 > /sys/devices/system/cpu/cpu7/core_ctl/busy_up_thres
-	echo 30 > /sys/devices/system/cpu/cpu7/core_ctl/busy_down_thres
-	echo 100 > /sys/devices/system/cpu/cpu7/core_ctl/offline_delay_ms
-	echo 1 > /sys/devices/system/cpu/cpu7/core_ctl/task_thres
+	echo 0 > /sys/devices/system/cpu/cpu6/core_ctl/min_cpus
+	echo 60 > /sys/devices/system/cpu/cpu6/core_ctl/busy_up_thres
+	echo 30 > /sys/devices/system/cpu/cpu6/core_ctl/busy_down_thres
+	echo 100 > /sys/devices/system/cpu/cpu6/core_ctl/offline_delay_ms
+	echo 1 > /sys/devices/system/cpu/cpu6/core_ctl/task_thres
 	# Controls how many more tasks should be eligible to run on gold CPUs
 	# w.r.t number of gold CPUs available to trigger assist (max number of
 	# tasks eligible to run on previous cluster minus number of CPUs in
@@ -5319,7 +5319,7 @@ case "$target" in
 	# Setting to 1 by default which means there should be at least
 	# 4 tasks eligible to run on gold cluster (tasks running on gold cores
 	# plus misfit tasks on silver cores) to trigger assitance from gold+.
-	echo 1 > /sys/devices/system/cpu/cpu7/core_ctl/nr_prev_assist_thresh
+	echo 1 > /sys/devices/system/cpu/cpu6/core_ctl/nr_prev_assist_thresh
 
 	# Disable Core control on silver
 	echo 0 > /sys/devices/system/cpu/cpu0/core_ctl/enable
@@ -5352,8 +5352,8 @@ case "$target" in
 	echo 1 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/pl
 
 	# configure input boost settings
-	echo "0:1324800" > /sys/devices/system/cpu/cpu_boost/input_boost_freq
-	echo 120 > /sys/devices/system/cpu/cpu_boost/input_boost_ms
+	echo "0" > /sys/devices/system/cpu/cpu_boost/input_boost_freq
+	echo 0 > /sys/devices/system/cpu/cpu_boost/input_boost_ms
 
 	# configure governor settings for gold cluster
 	echo "schedutil" > /sys/devices/system/cpu/cpufreq/policy4/scaling_governor
